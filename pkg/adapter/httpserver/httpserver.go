d4e3b39468bed85dce007e8fc8ef39d8700fff5d
Charles Tenorio <charles.tenorio.3@globalhitss.com.br>
2023-07-28 11:16:02 -0300

    alteracao no httpserver melhoria

--- pkg/adapter/httpserver/httpserver.go
+++ pkg/adapter/httpserver/httpserver.go
@@ -8,18 +8,26 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	// "github.com/rs/cors"
+	"github.com/KatanaSoftwareHope/go-commons-libs/core/config"
 )
 
-func New(r chi.Router, port string) *http.Server {
+func New(r chi.Router, conf *config.Config) *http.Server {
 
 	// handler := cors.Default().Handler(r)
 	// handler := cors.AllowAll().Handler(r)
 	handler := r
 
+	SRV_PORT := os.Getenv("SRV_PORT")
+	if SRV_PORT != "" {
+		conf.PORT = SRV_PORT
+	} else {
+		conf.PORT = "3000"
+	}
+
 	srv := &http.Server{
 		ReadTimeout:  10 * time.Second, // Aguarda 10 segundos
 		WriteTimeout: 10 * time.Second, // Responde em 10 segundos
-		Addr:         ":" + port,
+		Addr:         ":" + conf.PORT,
 		Handler:      handler,
 		ErrorLog:     log.New(os.Stderr, "logger: ", log.Lshortfile),
 	}
